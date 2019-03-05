ede446ff97094bdfa6684c6665b6a68bf63e36bf
Christian Huffman <cjh605@gmail.com>
2019-03-05 14:33:26 -0500

    Finished tutorial and added to readme

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -1,26 +1,31 @@
+// This is based off Colt Steele's Async Await video which can be watched here
+// https://www.youtube.com/watch?v=krAYA4rvbdA
+
 // What Is Async Await?
-// The Async function declaration defines an asynchronus function, which retuns an AsyncFunction
+// The Async function declaration defines an asynchronus function, which retuns an AsyncFunction object
 // An asynchronous function is a function which operates asynchronously via the event loop,
 // using an implicit Promise to return the results. But the syntax and structure of your code using
 // async functions is much more like using standard synchronous functions.
 
-// Regular Asynchronous functions with a call back
-resolveAfter2Seconds = () => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve("resolved");
-    }, 2000);
-  });
-};
-
 // Async Await Function
-asyncCall = async () => {
-  console.log("calling");
-  const result = await resolveAfter2Seconds();
-  console.log(result);
-};
 
-asyncCall();
+// resolveAfter2Seconds = () => {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve("resolved");
+//     }, 2000);
+//   });
+// };
+
+// Here is the async call
+
+// asyncCall = async () => {
+//   console.log("calling");
+//   const result = await resolveAfter2Seconds();
+//   console.log(result);
+// };
+
+// asyncCall();
 
 // Async Overview
 // 1. The async keyword is part of ES2017
@@ -31,15 +36,18 @@ asyncCall();
 // Showing How Async Keyword returns a promise
 // This looks like it should return the console log
 // Instead it will return a promise
-friendlyFn = async () => {
-  return `Hello! This is my message!`;
-};
+
+// friendlyFn = async () => {
+//   return `Hello! This is my message!`;
+// };
 
 // When we call FriendlyFn() by itself, it doesn't return our message but instead a promise
-friendlyFn();
+
+// friendlyFn();
 
 // We can then encapsulate that promise in a variable and use that to console log the message
-friendlyFn().then(msg => console.log(msg));
+
+// friendlyFn().then(msg => console.log(msg));
 
 // What About Rejections?
 
@@ -65,53 +73,188 @@ friendlyFn().then(msg => console.log(msg));
 // Think of the await keyword like a pause button
 
 // You Cannot Use Await in a non-async function
-// Ex. hi = async () => {
+// Ex. hi = () => {
 // await "Hello";
 // }
 
 // Here is an example of using a proper async await function
 
-getStarWarsData = async () => {
-  console.log(`Getting Star Wars Data`);
-  let movieData = await $.getJSON("https://swapi.co/api/films/");
-  // These lines do NOT run until
-  //   the promise is resolved!
-  console.log("All Done!");
-  console.log(movieData);
-};
+// getStarWarsData = async () => {
+//   console.log(`Getting Star Wars Data`);
+//   let movieData = await $.getJSON("https://swapi.co/api/films/");
+//   // These lines do NOT run until
+//   //   the promise is resolved!
+//   console.log("All Done!");
+//   console.log(movieData);
+// };
 
-getStarWarsData();
-
-// Another Example
-
-let starWars = {
-  genre: "sci-fi",
-  async logMovieData() {
-    let url = "https://swapi.co/api/films/";
-    let movieData = await $.getJSON(url);
-    console.log("Finished Getting Movie Data!");
-    console.log(movieData.results);
-  }
-};
-
-starWars.logMovieData();
+// getStarWarsData();
 
 // Another Example
 
-class Pokemon {
-  constructor(id) {
-    this.id = id;
-  }
+// let starWars = {
+//   genre: "sci-fi",
+//   async logMovieData() {
+//     let url = "https://swapi.co/api/films/";
+//     let movieData = await $.getJSON(url);
+//     console.log("Finished Getting Movie Data!");
+//     console.log(movieData.results);
+//   }
+// };
 
-  async logName() {
-    let url = `https://pokeapi.co/api/v2/pokemon/${this.id}/`;
-    let response = await $.getJSON(url);
-    console.log(`Our Pokemon is ${response.name}!`);
-  }
-}
+// starWars.logMovieData();
+
+// Another Example using classes and constructors
+
+// class Pokemon {
+//   constructor(id) {
+//     this.id = id;
+//   }
+
+//   async logName() {
+//     let url = `https://pokeapi.co/api/v2/pokemon/${this.id}/`;
+//     let response = await $.getJSON(url);
+//     console.log(`Our Pokemon is ${response.name}!`);
+//   }
+// }
 
 // Declare a new Pokemon with the ID 658
-let pokemon = new Pokemon(658);
+// let pokemon = new Pokemon(658);
 
 // Log that pokemons name
-pokemon.logName();
+// pokemon.logName();
+
+// Another Example Using github API and how to handle promise rejection
+
+// getUser = async user => {
+//   try {
+//     let url = `https://api.github.com/users/${user}`;
+//     let response = await $.getJSON(url);
+//     console.log(`${response.name}: ${response.bio}`);
+//   } catch (e) {
+//     console.log("User does not exist!");
+//   }
+// };
+
+// getUser("TheNoodleMoose"); Will console log my profile
+// getUser("rfioasfoa"); Will catch the error and console log that the user doesn't exist
+
+// Why Using Callbacks Can Become a Mess
+
+// Here we will create three api calls, in three different ways to show the differences
+
+// First Using callbacks
+
+let baseURL = "https://pokeapi.co/api/v2/pokemon";
+// // As you can see, this works but is nested and not very pretty.
+
+// $.getJSON(`${baseURL}/1/`, p1 => {
+//   console.log(`The First Pokemon is ${p1.name}`);
+//   $.getJSON(`${baseURL}/2/`, p2 => {
+//     console.log(`The Second Pokemon is ${p2.name}`);
+//     $.getJSON(`${baseURL}/3/`, p3 => {
+//       console.log(`The Third Pokemon is ${p3.name}`);
+//     });
+//   });
+// });
+
+// Second Using .then()
+// This is a little bit cleaner but still not as great as it could be
+
+// $.getJSON(`${baseURL}/1/`)
+//   .then(p1 => {
+//     console.log(`The first pokemon is ${p1.name}`);
+//     return $.getJSON(`${baseURL}/2/`);
+//   })
+//   .then(p2 => {
+//     console.log(`The Second pokemon is ${p2.name}`);
+//     return $.getJSON(`${baseURL}/3/`);
+//   })
+//   .then(p3 => {
+//     console.log(`The Third pokemon is ${p3.name}`);
+//     return $.getJSON(`${baseURL}/3/`);
+//   });
+
+//   Now finally the async await version
+// As you can see, this is a way cleaner and concise version of what we are trying to do
+// catchSomeOfEm = async () => {
+//   let baseURL = "https://pokeapi.co/api/v2/pokemon";
+//   let p1 = await $.getJSON(`${baseURL}/1/`);
+//   let p2 = await $.getJSON(`${baseURL}/2/`);
+//   let p3 = await $.getJSON(`${baseURL}/3/`);
+
+//   console.log(`The First pokemon ios ${p1.name}`);
+//   console.log(`The Second pokemon ios ${p2.name}`);
+//   console.log(`The Third pokemon ios ${p3.name}`);
+// };
+
+// catchSomeOfEm();
+
+// This is great because the request still fire sequentially
+// They are make each request completely independent!
+
+// But guess what, there is an even better way to do this!
+// This version is great, but we are making three different request which can be slow
+
+// Here is the better version
+
+// This version allows the request to all be made in parallel at the the same time
+// Instead of one after another, this can make a difference on larger apps
+// catchSomeOfEmParallel = async () => {
+//   let baseURL = "https://pokeapi.co/api/v2/pokemon";
+//   let p1Promise = $.getJSON(`${baseURL}/1/`);
+//   let p2Promise = $.getJSON(`${baseURL}/2/`);
+//   let p3Promise = $.getJSON(`${baseURL}/3/`);
+
+//   let p1 = await p1Promise;
+//   let p2 = await p2Promise;
+//   let p3 = await p3Promise;
+
+//   console.log(`The First pokemon ios ${p1.name}`);
+//   console.log(`The Second pokemon ios ${p2.name}`);
+//   console.log(`The Third pokemon ios ${p3.name}`);
+// };
+
+// It's hard to tell the difference but I will show another example so you can see the difference
+// catchSomeOfEmParallel();
+
+// Here you will be able to visibilly be able to see the difference in timing
+
+pause = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+doSomething = async () => {
+  console.log("Starting");
+  let p1 = await pause(1000);
+  let p2 = await pause(1000);
+  let p3 = await pause(1000);
+  console.log(`All Done!`);
+};
+
+// When running this, it takes a total of 3 seconds for All Done to fire!
+// This is because it has to await the first one to finish, and then the next, one and finally the third one before it can be All Done!
+
+doSomething();
+
+// Here is the better version to show the difference
+
+doSomethingBetter = async () => {
+  console.log("Better Version Starting");
+  let promise1 = pause(1000);
+  let promise2 = pause(1000);
+  let promise3 = pause(1000);
+
+  await promise1;
+  await promise2;
+  await promise3;
+  console.log("All Done Faster!!");
+};
+
+// When running this you can see it only takes one second to finish
+// This is because they all run sequentially instead of awaiting
+
+doSomethingBetter();
+
+// Overall, Async Await is a much simpler and cleaner way of returning promises in our code!
+// Hope You enjoyed and be sure to give this repo a star if you liked it!
--- README.md
+++ README.md
@@ -1,2 +1,141 @@
 # Async-Await-Examples
-This repo is for those looking to understand and learn more about Async Await functions and why to use them over callbacks or .then()
+
+## Hi there! And welcome to my repo on Async Await and why it will make your life better as a developer!
+
+### So, What Is Async Await?
+
+#### The Async function declaration defines an asynchronus function, which retuns an AsyncFunction object. An asynchronous function is a function which operates asynchronously via the event loop, using an implicit Promise to return the results. But the syntax and structure of your code using async functions is much more like using standard synchronous functions.
+
+### Let's Look At A Function Using Async Await
+
+![AsyncAwaitFunction](./assets/images/image1.PNG)
+
+### Here it is in action:
+
+![AsyncAwaitFunctionGIF](./assets/images/gif1.gif)
+
+### Async Overview
+
+1. The async keyword is part of ES2017
+2. You can declare any function in JavaScript as async
+3. Async functions always return promises
+4. Inside of an async fucntion, you can write code that looks synchronous, even if it isn't
+
+### Let's Look At How Async Functions Always Return a Promise
+
+#### This function looks like it should return our sentence, but instead will return a promise
+
+![ReturnAPromise](./assets/images/image2.PNG)
+
+#### We Can Encapsulate Our Promise in a variable and console log our message from that variable
+
+![ConsoleLogMessage](./assets/images/image3.PNG)
+
+#### Here it is in our console
+
+![ConsoleLoggedMessage](./assets/images/image4.PNG)
+
+### What About Rejections?
+
+#### Inside of async functions, the return value is wrapped in a resolved promise. If you want to reject instead of resolve, simply throw an error inside of the async function
+
+#### Here Is An Example of a Rejected Function:
+
+![RejectionHandling](./assets/images/image5.PNG)
+
+#### Inside of our console we see:
+
+![UncaughtPromise](./assets/images/image6.PNG)
+
+#### We Can capture this error to log the message instead by using .catch():
+
+![CatchError](./assets/images/image7.PNG)
+
+#### Now in the console we will see a console log of our error message along with the error:
+
+![CatchErrorConsoleLog](./assets/images/image8.PNG)
+
+### The Await Overview
+
+- Inside of an async function, we can use the await keyword
+- Await pauses the execution of the async function
+- Can await any async operation returning a promise(Ex. other async function!)
+- The await keyword waits for promise to resolve & Extracts its resolved value
+- It then resumes the async function's execution
+- Think of the await keyword like a pause button
+
+#### YOU CANNOT USE AWAIT WITH A NON ASYNC FUNCTION
+
+Incorrect:
+
+![NonAsyncFunction](./assets/images/image9.PNG)
+
+Correct:
+
+![CorrectAsyncFunction](./assets/images/image10.PNG)
+
+### Here Is An Example Of A Proper Async Await Function:
+
+![CorrectAsyncFunction](./assets/images/image11.PNG)
+
+#### Here It Is In Action:
+
+![StarwarsAPICall](./assets/images/gif2.gif)
+
+### So, Why Is This Important?
+
+#### As you can see from the last example, all the code after await does not run until await is fired. This can be very important if you have code you don't wish to run until something is finished(such as a api call or database query). THINK OF AWAIT AS A PAUSE BUTTON!
+
+## Now Let's See Why Async Await is better than Callbacks or .then()
+
+#### I'm going to show three functions that all do the same thing, but are coded in different ways. The visual difference will be stunning!
+
+#### First: Callbacks
+
+![PokemonCallbacks](./assets/images/image12.PNG)
+
+#### Second: .then()
+
+Not as bad as the last one, but still can become messy on larger applications
+
+![PokemonDotThen](./assets/images/image13.PNG)
+
+#### Third: Async Await
+
+![PokemonAsyncAwait](./assets/images/image14.PNG)
+
+#### Here Is Our Code In Action:
+
+![PokemonAPIAsyncAwait](./assets/images/gif3.gif)
+
+#### This is Great! Our code fires sequentially in order just fine! But This Could be Even Better and Faster! Which is important for larger scales applications!
+
+### Here Is Our Better Version!
+
+![BetterPokemonAsyncAwait](./assets/images/image15.PNG)
+
+#### Now you may be asking, "What Is The Difference?" In this code, you wouldn't really be able to tell a difference. They would fire just like the other code did. But let me show you with a different example on why this is truly a faster and better way to code this api call.
+
+#### Here Is Function That Runs Similar to the Non-better Version of the Pokemon Async Await Function
+
+![AsyncTimer](./assets/images/image16.PNG)
+
+#### Here It Is Running:
+
+You can see that it takes a total of three seconds for all done to console log. This is because it has to await the finish of all three pause functions(1 second each) in order to fire the console log.
+
+![TimerGif](./assets/images/gif4.gif)
+
+#### Now Here Is The Better Version Of This Code:
+
+![BetterAsyncTimer](./assets/images/image17.PNG)
+
+#### Now Let's Watch Both Run Together To See The Difference!
+
+![SameTimeTimerGif](./assets/images/gif5.gif)
+
+#### As you can see, the better version only took one second to fire instead of three! Making your actual request not be awaits and instead allow them to fire in parallel with each other improves asynchronicity overall!
+
+# The End
+
+### I really hope you guys enjoyed this and learned something from it! If you could take the time to leave a star that would be appreciated!
